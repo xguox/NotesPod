5254e02a6aeea935f4e24b12536ec3e3c8d05216
xguox <xxgguuooxx@gmail.com>
2016-06-17 15:52:04 +0800

    NoteDetail

--- app/src/main/java/me/xguox/notespod/MainActivityListFragment.java
+++ app/src/main/java/me/xguox/notespod/MainActivityListFragment.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
--- app/src/main/java/me/xguox/notespod/NoteDetailActivity.java
+++ app/src/main/java/me/xguox/notespod/NoteDetailActivity.java
@@ -1,5 +1,7 @@
 package me.xguox.notespod;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,17 @@ public class NoteDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_detail);
+        createAndAddFragment();
+    }
+
+    private void createAndAddFragment() {
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        NoteViewFragment noteViewFragment = new NoteViewFragment();
+        setTitle(R.string.viewFragmentTitle);
+        fragmentTransaction.add(R.id.note_container, noteViewFragment, "NOTE_VIEW_FRAGMENT");
+
+        fragmentTransaction.commit();
     }
 }
