8593045cfadefd71984782b86b5a7eec83546190
xguox <xxgguuooxx@gmail.com>
2016-06-19 20:29:55 +0800

    Preferences

--- app/src/main/java/me/xguox/notespod/MainActivity.java
+++ app/src/main/java/me/xguox/notespod/MainActivity.java
@@ -1,7 +1,11 @@
 package me.xguox.notespod;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +13,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        loadPreferences();
     }
 
     @Override
@@ -44,6 +50,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, AppPreferences.class);
+            startActivity(intent);
             return true;
         } else if (id == R.id.action_add_note) {
             Intent intent = new Intent(this, NoteDetailActivity.class);
@@ -53,5 +61,20 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void loadPreferences() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        boolean isBackgroundDark = sharedPreferences.getBoolean("background_color", false);
+
+        if (isBackgroundDark) {
+            LinearLayout mainLayout = (LinearLayout) findViewById(R.id.mainActivityLayout);
+            mainLayout.setBackgroundColor(Color.parseColor("#001F3F"));
+        }
+
+        String notebookTitle = sharedPreferences.getString("title", "Notespod");
+        setTitle(notebookTitle);
+
     }
 }
